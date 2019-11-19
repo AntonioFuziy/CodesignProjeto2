59e2968876f0531fbf3b4bf08ae0306e9abf7e08
AntonioFuziy <49486453+AntonioFuziy@users.noreply.github.com>
2019-11-19 17:08:56 -0300

    Add files via upload

---
+++ assets/js/cliente.js
@@ -0,0 +1,55 @@
+let menu = document.getElementById('menu-hover')
+
+let oneStar = document.getElementById('one-star')
+let twoStar = document.getElementById('two-star')
+let threeStar = document.getElementById('three-star')
+let fourStar = document.getElementById('four-star')
+let fiveStar = document.getElementById('five-star')
+
+function paginaInicial(){
+    if (menu.style.display == 'none'){
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+}
+
+function starFillClickOne(){
+    oneStar.src = 'assets/img/fill-star.png'
+    twoStar.src = 'assets/img/star.png'
+    threeStar.src = 'assets/img/star.png'
+    fourStar.src = 'assets/img/star.png'
+    fiveStar.src = 'assets/img/star.png'
+}
+
+function starFillClickTwo(){
+    oneStar.src = 'assets/img/fill-star.png'
+    twoStar.src = 'assets/img/fill-star.png'
+    threeStar.src = 'assets/img/star.png'
+    fourStar.src = 'assets/img/star.png'
+    fiveStar.src = 'assets/img/star.png'
+}
+
+function starFillClickThree(){
+    oneStar.src = 'assets/img/fill-star.png'
+    twoStar.src = 'assets/img/fill-star.png'
+    threeStar.src = 'assets/img/fill-star.png'
+    fourStar.src = 'assets/img/star.png'
+    fiveStar.src = 'assets/img/star.png'
+}
+
+function starFillClickFour(){
+    oneStar.src = 'assets/img/fill-star.png'
+    twoStar.src = 'assets/img/fill-star.png'
+    threeStar.src = 'assets/img/fill-star.png'
+    fourStar.src = 'assets/img/fill-star.png'
+    fiveStar.src = 'assets/img/star.png'
+}
+
+function starFillClickFive(){
+    oneStar.src = 'assets/img/fill-star.png'
+    twoStar.src = 'assets/img/fill-star.png'
+    threeStar.src = 'assets/img/fill-star.png'
+    fourStar.src = 'assets/img/fill-star.png'
+    fiveStar.src = 'assets/img/fill-star.png'
+}
